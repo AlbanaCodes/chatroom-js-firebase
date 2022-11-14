a99cd0036af9ed5de4031ce153972178a5f37b09
Albana <28781490+AlbanaCodes@users.noreply.github.com>
2022-11-14 20:31:23 +0100

    firestore and config

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js';
+import {
+    getFirestore, collection, query,
+    getDocs, where, orderBy, setDoc, doc,
+    deleteDoc
+} from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
+
+const config = new Config();
+
+const firebaseConfig = {
+    apiKey: config.apiKey,
+    authDomain: config.authDomain,
+    projectId: config.projectId,
+    storageBucket: config.storageBucket,
+    messagingSenderId: config.messagingSenderId,
+    appId: config.appId,
+    measurementId: config.measurementId
+  };
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+console.log('firebaseApp: ', firebaseApp);
+console.log('db: ', db);
+window.firebasedb = db;
---
+++ scripts/config.js
@@ -0,0 +1,11 @@
+class Config{
+  constructor(){
+    this.apiKey = "";
+    this.authDomain = "";
+    this.projectId = "";
+    this.storageBucket = "";
+    this.messagingSenderId = "";
+    this.appId = "";
+    this.measurementId = "";
+  }
+}
