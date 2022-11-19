2beac705ef4040740d17c810ceca474cdeb01865
Albana <28781490+AlbanaCodes@users.noreply.github.com>
2022-11-19 20:04:18 +0100

    update username

--- scripts/app.js
+++ scripts/app.js
@@ -10,6 +10,7 @@ let selectedRoomBtn = document.querySelector('.btn_active');
 let selectedRoom = selectedRoomBtn.innerText.slice(1);
 console.log('selectedRoomBtn: ', selectedRoomBtn);
 console.log('selectedRoom: ', selectedRoom);
+const usernameInput = document.getElementById('username');
 
 const firebaseConfig = {
   apiKey: config.apiKey,
@@ -34,7 +35,9 @@ window.addEventListener('DOMContentLoaded', (e) => {
     snapshot.docs.forEach((doc) => {
       chats.push({...doc.data()});
     });
-    console.log('chats: ', chats);
+    console.log('chats: ', chats); // we get the added chat twice in the log. this happens because of the timestamp
+    // created_at. becaue we add the document in the addMsgForm submit event, and when it gets the server time
+    // it has a difference and considers it as edited. and it returns it as such (edit). that's why in shows the log twice
   });
 });
 
@@ -43,7 +46,7 @@ addMsgForm.addEventListener('submit', e => {
   e.preventDefault();
   
   addDoc(chatsRef, {
-    username: usernameStrong.textContent,
+    username: usernameStrong.innerHTML,
     message: chatMsgInpt.value,
     room: selectedRoom,
     created_at: serverTimestamp ()
@@ -52,4 +55,15 @@ addMsgForm.addEventListener('submit', e => {
     addMsgForm.reset();
   })
   .catch(err => console.log('error while adding document: ', err.message));
+});
+
+const updateUsername = (username) => {
+  usernameStrong.innerHTML = username;
+};
+
+const editUsernameForm = document.querySelector('.my-name');
+editUsernameForm.addEventListener('submit', e => {
+  e.preventDefault();
+  updateUsername(usernameInput.value);
+  editUsernameForm.reset();
 });
